be53ea556b69a6acc6b063676179ecdedd8ffaa9
Isaiah Simon <helpline@Helplines-iMac-2.local>
2016-03-04 16:31:20 -0500

    Problem 5 Initial Commit

---
+++ Chapter 1/Problem5.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.StringBuilder;
+
+public class Problem5 {
+     
+    public static boolean oneEdit(String s, String t){
+    
+      return true;
+    }
+    public static void main(String[] args) {
+    }
+}
