5fc7598172457e387b08bad77c6dedf86be4e24e
Isaiah Simon <helpline@Helplines-iMac-2.local>
2016-03-04 13:48:53 -0500

    SpaceCount Creation

--- Chapter 1/Problem3.java
+++ Chapter 1/Problem3.java
@@ -3,10 +3,28 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+import java.lang.StringBuilder;
 
 public class Problem3 {
     
+    public static String urlify(String s, int n){
+       int spaceCount = 0, i;
+       char[] chars = s.toCharArray();  
+       
+       for(i = 0; i<s.length(); i++){
+         if(chars[i] == ' '){
+            spaceCount++;
+         }
+       }
+       
+       System.out.println(spaceCount);
+       
+       return s;
+    }  
+    
     public static void main(String[] args) {
+    
+    System.out.println(urlify("This is a string", 16));
 
     }
 }
