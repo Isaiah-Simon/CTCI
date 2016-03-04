2a89f5dcb4388c868725841039a6ee64ce83ea77
Isaiah Simon <helpline@Helplines-iMac-2.local>
2016-03-04 13:25:18 -0500

    Initial Problem 3 Creation

---
+++ Chapter 1/Problem3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Problem3 {
+    
+    public static void main(String[] args) {
+
+    }
+}
